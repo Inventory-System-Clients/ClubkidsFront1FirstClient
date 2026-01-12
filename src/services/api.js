782819d425d2346d3d72d00318d29bc36be3f61a
Gabriel <gakirakiyohara@gmail.com>
2026-01-12 15:15:51 -0300

    fix: api.js 3

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,9 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL:
-    import.meta.env.VITE_API_URL ||
-    "https://clubekids1firstclient.onrender.com/api",
+    (import.meta.env.VITE_API_URL ||
+      "https://clubekids1firstclient.onrender.com") +
+    (import.meta.env.VITE_API_URL ? "" : "/api"),
   headers: {
     "Content-Type": "application/json",
   },
